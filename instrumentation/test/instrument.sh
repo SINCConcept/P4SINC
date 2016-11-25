5e9dafd39ff25389b228e5998974447390880b15
Phu Phung <phunghuuphu@gmail.com>
2016-11-25 08:58:11 -0500

    update script and libs

--- instrumentation/test/instrument.sh
+++ instrumentation/test/instrument.sh
@@ -1,6 +1,8 @@
-#note: ensure that policy.json api-map.json are ready in the current directory
-#usage: ./instrument.sh <app-jar to be instrumented>
-#output: Instrumented-App.jar
+echo "note: ensure that policy.json api-map.json are ready in the current directory"
+echo "usage: ./instrument.sh <app-jar to be instrumented>"
+echo "Compiling the policies..."
 java -jar tools/instrument.jar 
-tools/ajc -cp .:tools/aspectjrt.jar -inpath $1 -outjar Instrumented-App.jar output.aj
+echo "Instrumenting the app..."
+java -cp .:tools/aspectjtools.jar org.aspectj.tools.ajc.Main -cp .:tools/aspectjrt.jar -inpath $1 -outjar Instrumented-App.jar output.aj
+echo "instrumented app: Instrumented-App.jar"
 rm output.aj
